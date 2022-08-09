8b8c211ed11ac9e0becf04df6baf61761bc17dcb
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-08-09 08:20:51 +0500

    Added New Problems

--- 1) Implementation of DFS/Main.java
+++ 1) Implementation of DFS/Main.java
@@ -68,10 +68,11 @@ class DFSTraversal{
 
         int step = 1;
 
-        System.out.println("Step # " + step);
-        System.out.println("Current Node: " + currentNode);
-        System.out.println("Stack: " + stack);
-        System.out.println("Visited: " + Arrays.toString(visitedNodes));
+        System.out.println("\tStep # " + step);
+        System.out.println("\tCurrent Node: " + currentNode);
+        System.out.println("\tStack: " + stack);
+        System.out.println("\tVisited: " + Arrays.toString(visitedNodes));
+        
         System.out.println();
 
         // Repeat Loop until Stack is empty
@@ -88,10 +89,10 @@ class DFSTraversal{
                     visitedNodes[++index] = currentNode.getChildren()[i].getItem();
                     currentNode = currentNode.getChildren()[i];
 
-                    System.out.println("Step # " + step);
-                    System.out.println("Current Node: " + currentNode);
-                    System.out.println("Stack: " + stack);
-                    System.out.println("Visited: " + Arrays.toString(visitedNodes));
+                    System.out.println("\tStep # " + step);
+                    System.out.println("\tCurrent Node: " + currentNode);
+                    System.out.println("\tStack: " + stack);
+                    System.out.println("\tVisited: " + Arrays.toString(visitedNodes));
                     System.out.println();
 
                     continue l;
@@ -119,10 +120,10 @@ class DFSTraversal{
             //currentNode = stack.pop();
            // stack.remove(currentNode);
     
-           System.out.println("Step # " + step);
-           System.out.println("Current Node: " + currentNode);
-           System.out.println("Stack: " + stack);
-           System.out.println("Visited: " + Arrays.toString(visitedNodes));
+           System.out.println("\tStep # " + step);
+           System.out.println("\tCurrent Node: " + currentNode);
+           System.out.println("\tStack: " + stack);
+           System.out.println("\tVisited: " + Arrays.toString(visitedNodes));
            System.out.println();
 
         }
@@ -131,7 +132,7 @@ class DFSTraversal{
 
     }
 
-    static String limitedDFSTraverse(Node rootNode, int size, int level){
+    static String limitedDepthSearchTraverse(Node rootNode, int size, int level){
 
         Stack <Node> stack = new Stack<Node>(); 
 
@@ -149,10 +150,10 @@ class DFSTraversal{
 
         int step = 1;
 
-        System.out.println("Step # " + step);
-        System.out.println("Current Node: " + currentNode);
-        System.out.println("Stack: " + stack);
-        System.out.println("Visited: " + Arrays.toString(visitedNodes));
+        System.out.println("\tStep # " + step);
+        System.out.println("\tCurrent Node: " + currentNode);
+        System.out.println("\tStack: " + stack);
+        System.out.println("\tVisited: " + Arrays.toString(visitedNodes));
         System.out.println();
 
         // Repeat Loop until Stack is empty
@@ -171,10 +172,10 @@ class DFSTraversal{
                     visitedNodes[++index] = currentNode.getChildren()[i].getItem();
                     currentNode = currentNode.getChildren()[i];
 
-                    System.out.println("Step # " + step);
-                    System.out.println("Current Node: " + currentNode);
-                    System.out.println("Stack: " + stack);
-                    System.out.println("Visited: " + Arrays.toString(visitedNodes));
+                    System.out.println("\tStep # " + step);
+                    System.out.println("\tCurrent Node: " + currentNode);
+                    System.out.println("\tStack: " + stack);
+                    System.out.println("\tVisited: " + Arrays.toString(visitedNodes));
                     System.out.println();
 
                     continue l;
@@ -201,10 +202,10 @@ class DFSTraversal{
             //currentNode = stack.pop();
            // stack.remove(currentNode);
     
-           System.out.println("Step # " + step);
-           System.out.println("Current Node: " + currentNode);
-           System.out.println("Stack: " + stack);
-           System.out.println("Visited: " + Arrays.toString(visitedNodes));
+           System.out.println("\tStep # " + step);
+           System.out.println("\tCurrent Node: " + currentNode);
+           System.out.println("\tStack: " + stack);
+           System.out.println("\tVisited: " + Arrays.toString(visitedNodes));
            System.out.println();
 
         }
@@ -238,21 +239,24 @@ public class Main{
         
         int size = 0;
 
-        // First Problem # 1
+        // PROBLEM # 1
+
+        // Define Nodes
         Node f = new Node("F");
         Node b = new Node("B");
         Node g = new Node("G");
-        Node index = new Node("index");
+        Node a = new Node("A");
         Node d = new Node("D");
         Node i = new Node("I");
         Node c = new Node("C");
         Node e = new Node("E");
         Node h = new Node("H");
         
+        // Set Children
         f.setAllChildren(new Node[]{b,g});
-        b.setAllChildren(new Node[]{index,d});
+        b.setAllChildren(new Node[]{a,d});
         g.setAllChildren(new Node[]{i});
-        index.setAllChildren(new Node[]{});
+        a.setAllChildren(new Node[]{});
         d.setAllChildren(new Node[]{c,e});
         i.setAllChildren(new Node[]{h});
         c.setAllChildren(new Node[]{});
@@ -260,12 +264,13 @@ public class Main{
         h.setAllChildren(new Node[]{});
         
         size = 9;
+        System.out.println("\n*********** PROBLEM # 1 *********** ");
+        System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(f, size));
+        //System.out.println("FINAL Limited TRAVERSING ORDER: " + DFSTraversal.limitedTraverse(f, size,2));
 
-        // System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(f, size));
-        //System.out.println(" Final Limited TRAVERSING ORDER: " + DFSTraversal.limitedTraverse(f, size,2));
-
-        // Sec
-        // Create Nodes
+        // PROBLEM # 2
+        
+        // Define Nodes
         Node rootNode = new Node("S");
 
         Node sNode = new Node("Q");
@@ -293,70 +298,150 @@ public class Main{
         siNode.setAllChildren(new Node[]{});
 
         size = 6;
+        System.out.println("\n*********** PROBLEM # 2 *********** ");
         System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(rootNode, size));
 
-        System.out.println(" Final Limited TRAVERSING ORDER: " + DFSTraversal.limitedDFSTraverse(rootNode, size,2));
+        // PROBLEM # 3
 
+        // Define Nodes
+        f = new Node("F");
+        b = new Node("B");
+        g = new Node("G");
+        a = new Node("A");
+        d = new Node("D");
+        i = new Node("I");
+        c = new Node("C");
+        e = new Node("E");
+        h = new Node("H");
+        Node j = new Node("J");
+        Node k = new Node("K");
+        Node l = new Node("L");
+        Node m = new Node("M");
 
+        // Set Children
+        a.setAllChildren(new Node[]{b,c,d});
+        b.setAllChildren(new Node[]{e});
+        c.setAllChildren(new Node[]{f,g,h});
+        d.setAllChildren(new Node[]{i,j});
+        e.setAllChildren(new Node[]{k,l});
+        f.setAllChildren(new Node[]{});
+        g.setAllChildren(new Node[]{});
+        h.setAllChildren(new Node[]{m});
+        i.setAllChildren(new Node[]{});
+        j.setAllChildren(new Node[]{});
+        k.setAllChildren(new Node[]{});
+        l.setAllChildren(new Node[]{});
+        m.setAllChildren(new Node[]{});
         
-        // THIRD Problem
-        // Node f = new Node("F");
-        // Node b = new Node("B");
-        // Node g = new Node("G");
-        // Node index = new Node("index");
-        // Node d = new Node("D");
-        // Node i = new Node("I");
-        // Node c = new Node("C");
-        // Node e = new Node("E");
-        // Node h = new Node("H");
-        // Node j = new Node("J");
-        // Node k = new Node("K");
-        // Node l = new Node("L");
-        // Node m = new Node("M");
+        size = 13;
+        System.out.println("\n*********** PROBLEM # 3 *********** ");
+        System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(a, size));
 
-        // index.setAllChildren(new Node[]{b,c,d});
-        // b.setAllChildren(new Node[]{e});
-        // c.setAllChildren(new Node[]{f,g,h});
-        // d.setAllChildren(new Node[]{i,j});
-        // e.setAllChildren(new Node[]{k,l});
-        // f.setAllChildren(new Node[]{});
-        // g.setAllChildren(new Node[]{});
-        // h.setAllChildren(new Node[]{m});
-        // i.setAllChildren(new Node[]{});
-        // j.setAllChildren(new Node[]{});
-        // k.setAllChildren(new Node[]{});
-        // l.setAllChildren(new Node[]{});
-        // m.setAllChildren(new Node[]{});
+        // PROBLEM # 4
+
+        // Define Node
+        Node eig = new Node("8");
+        Node thr = new Node("3");
+        Node ten = new Node("10");
+        Node one = new Node("1");
+        Node six = new Node("6");
+        Node ftn = new Node("14");
+        Node fou = new Node("4");
+        Node sev = new Node("7");
+        Node thn = new Node("13");
+
+        // Set Chidren
+        eig.setAllChildren(new Node[]{thr,ten});
+        thr.setAllChildren(new Node[]{one, six});
+        ten.setAllChildren(new Node[]{ftn});
+        one.setAllChildren(new Node[]{});
+        six.setAllChildren(new Node[]{fou, sev});
+        ftn.setAllChildren(new Node[]{thn});
+        fou.setAllChildren(new Node[]{});
+        sev.setAllChildren(new Node[]{});
+        thn.setAllChildren(new Node[]{});
         
-        // size = 13;
+        size = 9;
+        System.out.println("\n*********** PROBLEM # 4 *********** ");
+        System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(eig, size));
 
-        // // System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(index, size));
+        // No Solution in Slides 
 
-        // Node eig = new Node("8");
-        // Node thr = new Node("3");
-        // Node ten = new Node("10");
-        // Node one = new Node("1");
-        // Node six = new Node("6");
-        // Node ftn = new Node("14");
-        // Node fou = new Node("4");
-        // Node sev = new Node("7");
-        // Node thn = new Node("13");
+        // Problem # 5
 
-        // eig.setAllChildren(new Node[]{thr,ten});
-        // thr.setAllChildren(new Node[]{one, six});
-        // ten.setAllChildren(new Node[]{ftn});
-        // one.setAllChildren(new Node[]{});
-        // six.setAllChildren(new Node[]{fou, sev});
-        // ftn.setAllChildren(new Node[]{thn});
-        // fou.setAllChildren(new Node[]{});
-        // sev.setAllChildren(new Node[]{});
-        // thn.setAllChildren(new Node[]{});
+        // Define Nodes
+        one = new Node("1");
+        Node two = new Node("2");
+        thr = new Node("3");
+        fou = new Node("4");
+        Node fiv = new Node("5");
+        six = new Node("6");
+        sev = new Node("7");
+        eig = new Node("8");
+        Node nin = new Node("9");
+        ten = new Node("10");
+        Node ele = new Node("11");
+        Node twe = new Node("12");
+
+        // Set Chidren
+        one.setAllChildren(new Node[]{two, sev, eig});
+        two.setAllChildren(new Node[]{thr, six});
+        thr.setAllChildren(new Node[]{fou, fiv});
+        fou.setAllChildren(new Node[]{});
+        fiv.setAllChildren(new Node[]{});
+        six.setAllChildren(new Node[]{});
+        sev.setAllChildren(new Node[]{});
+        eig.setAllChildren(new Node[]{nin, twe});
+        nin.setAllChildren(new Node[]{ten, ele});
+        ten.setAllChildren(new Node[]{});
+        ele.setAllChildren(new Node[]{});
+        twe.setAllChildren(new Node[]{});
+
+        size = 12;
+        System.out.println("\n*********** PROBLEM # 5 *********** ");
+        System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(one, size));
+
+
+        // ##############################################################
+        System.out.println("\n\n*************************** DEPTH LIMITED SEARCH ***************************");
         
-        // size = 9;
+        // PROBLEM # 1
 
-        // System.out.println("FINAL TRAVERSING ORDER: " + DFSTraversal.traverse(eig, size));
+        // Set Children
+        f.setAllChildren(new Node[]{b,g});
+        b.setAllChildren(new Node[]{a,d});
+        g.setAllChildren(new Node[]{i});
+        a.setAllChildren(new Node[]{});
+        d.setAllChildren(new Node[]{c,e});
+        i.setAllChildren(new Node[]{h});
+        c.setAllChildren(new Node[]{});
+        e.setAllChildren(new Node[]{});
+        h.setAllChildren(new Node[]{});
 
+        size = 9;
+        System.out.println("\n*********** PROBLEM # 1 *********** ");
+        System.out.println("FINAL Limited TRAVERSING ORDER: " + DFSTraversal.limitedDepthSearchTraverse(f, size,2));
+   
 
+        // PROBLEM # 2
+
+        // Set Children
+        rootNode.setAllChildren(new Node[] {sNode, tNode, fNode});
+
+        sNode.setAllChildren(new Node[]{fiNode, tNode});
+
+        tNode.setAllChildren(new Node[]{fNode, siNode});
+
+        fNode.setAllChildren(new Node[]{siNode});
+
+        // siNode, tNode
+        fiNode.setAllChildren(new Node[]{ siNode, tNode});
+
+        siNode.setAllChildren(new Node[]{});
+
+        size = 6;
+        System.out.println("\n*********** PROBLEM # 2 *********** ");
+        System.out.println(" Final Limited TRAVERSING ORDER: " + DFSTraversal.limitedDepthSearchTraverse(rootNode, size,2));
         
     }
 
