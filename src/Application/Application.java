ca33943b93b2f8a4203ab05bbd4f47bb2aba99d0
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-08-29 10:18:10 +0500

    Added Portable App to Use Program without installing JVM or e.t.c

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,10 +1,6 @@
 package Application;
 
-import com.informedsearchalgorithms.astarsearchalgorithm.algorithm.AStarSearchAlgorithm;
-import com.informedsearchalgorithms.astarsearchalgorithm.examples.AStarSearchAlgorithmExamples;
-import com.informedsearchalgorithms.greedybestfirstsearchalgorithm.examples.GreedyBestFirstSearchAlgorithmExamples;
 import com.project.ui.UI;
-import com.uninformedsearchalgorithms.breathfirstsearchalgorithm.examples.BreadthFirstSearchAlgoritmExamples;
 
 public class Application {
 
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module dependencies {
+	requires java.desktop;
+}
